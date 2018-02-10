73d62da8558efdd72ba7e97512a13697be084399
Irish Jingco <irishjingco@gmail.com>
2018-02-10 12:45:00 +0800

    Updated database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
 
 module.exports = {
- uri: 'mongodb://localhost:27017/meandb', // Databse URI and database name
+ uri: 'mongodb://usersample:Usersample25@ds117758.mlab.com:17758/meandb', // Databse URI and database name
+ //mongodb://localhost:27017/meandb
  secret: crypto, // Cryto-created secret
  db: 'meandb' // Database name
 }
